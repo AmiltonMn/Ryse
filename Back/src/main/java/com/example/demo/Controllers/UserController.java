341b5f5182bd7bb0e54c8eb32f40ec20222d0930
akauzinha47 <kau.vmenezes@gmail.com>
2024-12-04 11:15:56 -0300

    login ok (not tested)

--- Back/src/main/java/com/example/demo/Controllers/UserController.java
+++ Back/src/main/java/com/example/demo/Controllers/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.DTO.LoginData;
+import com.example.demo.DTO.LoginReturn;
 import com.example.demo.DTO.RegisterData;
 import com.example.demo.Services.UserServices;
 
@@ -21,6 +23,16 @@ public class UserController {
         System.err.println("entrou");
         String response = userServices.register(data);
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginReturn> Login(@RequestBody LoginData data){
+
+        var response = userServices.Login(data);
+
+        return response.sucess()? 
+            new ResponseEntity<>(response, HttpStatus.OK) : 
+            new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
 }
--- Back/src/main/java/com/example/demo/Services/UserServices.java
+++ Back/src/main/java/com/example/demo/Services/UserServices.java
@@ -1,10 +1,11 @@
 package com.example.demo.Services;
 
 import com.example.demo.DTO.LoginData;
+import com.example.demo.DTO.LoginReturn;
 import com.example.demo.DTO.RegisterData;
 
 public interface UserServices {
   String register(RegisterData data);
-  String Login(LoginData data);
+  LoginReturn Login(LoginData data);
   Boolean checkPassword(String password);
 }
