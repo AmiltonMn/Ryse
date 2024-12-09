d1578be0541d71b8d43ec0928af69e630315c196
juupurcino <jdiaspurcino@gmail.com>
2024-12-09 16:52:10 -0300

    Perfil quase

--- Front/src/components/selectProfile.tsx
+++ Front/src/components/selectProfile.tsx
@@ -1,10 +1,9 @@
-
-
-export const SelectProfile = ({ title, refe}: { title: string; refe: string}) => {
+export const SelectProfile = ({ title, refe, click, classe}: { title: string; refe: string; click: Function; classe: string}) => {
   return (
-    <a href={refe} className="relative inline-block overflow-hidden group mr-8">
+    <a href={refe} className="relative inline-block overflow-hidden group mr-8"  onClick={(event) => { event.preventDefault(); 
+      click();}}>
       <span className="relative z-10">{title}</span>
-      <span className="absolute left-1/2 bottom-0 w-0 h-[3px] bg-[#F41C54] transform transition-all duration-300 group-hover:w-full group-hover:left-0 focus:w-full focus:left-0"></span>
+      <span className={`absolute left-1/2 bottom-0 w-0 h-[3px] bg-[#F41C54] transform transition-all duration-300 group-hover:w-full group-hover:left-0 focus:w-full focus:left-0 ${classe ? classe : "" } ${classe ? "w-full -left-0" : "" }`} ></span>
     </a>
   );
 };
--- Front/src/components/topicArea.tsx
+++ Front/src/components/topicArea.tsx
@@ -1,15 +1,25 @@
-export const TopicArea = ({ text }: {
+import del from "@/assets/delete.png";
+import delBranco from "@/assets/deletebranco.png";
+import Image from "next/image";
+
+export const TopicArea = ({ text, refe }: {
 
     text: string;
+    refe: string;
 
 }) => {
 
     return (
 
         <>
-        <div>
-            <h1 className="font-light text-[16px]">• {text}</h1>   
-        </div>
+            <div className="group/item flex flex-row border-0 rounded-[10px] justify-between mr-20 hover:bg-[#292929] p-2 items-center">
+                <div>
+                    <h1 className="font-light text-[16px]">• {text}</h1>
+                </div>
+                <a href={refe} className="group/edit invisible group-hover/item:visible transition duration-300 ease-in-out scale-100 flex flex-row hover:bg-[#383838] p-1 px-2 border-0 rounded-lg">
+                    <Image src={del.src} width={20} height={20} alt="Delete area of interest"></Image>
+                </a>
+            </div>
         </>
     );
 
