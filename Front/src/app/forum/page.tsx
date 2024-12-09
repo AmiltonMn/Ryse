b6e11fd304e28f001fe6af5b3539de9f88b9001b
ingridcriistine <ingridrochacristine@gmail.com>
2024-12-09 10:55:35 -0300

    modais

--- Front/src/app/forum/page.tsx
+++ Front/src/app/forum/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { CardQuestion } from "@/components/cardQuestion";
 import { Menu } from "@/components/menu";
 import { Submenu } from "@/components/submenu";
@@ -18,24 +20,40 @@ const styles = {
 
 export default function Home() {
 
+    const [modal, setModal] = useState(false);
+    const [title, setTitle] = useState<string>("");
+    const [text, setText] = useState<string>("");
+    const [topic, setTopic] = useState<string>("");
+
+    const closeModal = () => {
+        setTitle("");
+        setText("");
+        setTopic("");
+        setModal(false);
+    }
+
+    const openModal = () => {
+        setModal(true);
+    }
+
     return (
         <div>
-            <Menu title={"Ryse"}/>
-            <Submenu home={"Home"} chats={"Chats"} newGroup={"New group"} myGroup={"My groups"} chatPrincipal1={"Chat 1"} chatPrincipal2={"Chat 2"} chatPrincipal3={"Chat 3"} newIdea={"New idea"} ideas={"Ideas"}/>
+            <Menu title={"Ryse"} />
+            <Submenu home={"Home"} chats={"Chats"} newGroup={"New group"} myGroup={"My groups"} chatPrincipal1={"Chat 1"} chatPrincipal2={"Chat 2"} chatPrincipal3={"Chat 3"} newIdea={"New idea"} ideas={"Ideas"} />
             <div className="pt-[180px] pl-[300px] flex">
                 <div className="w-[97%]">
                     <div className="flex justify-between items-center">
                         <h2 className="text-white font-bold text-[25px] mb-6">Java project forum</h2>
-                        <Link href={ROUTES.home} className={styles.button}>
-                            <Image src={iconMore} alt="ícone mais" className={styles.img}/>
+                        <button onClick={() => openModal()} className={styles.button}>
+                            <Image src={iconMore} alt="ícone mais" className={styles.img} />
                             New question
-                        </Link>
+                        </button>
                     </div>
-                    <hr/>
+                    <hr />
                     <div className="w-full pt-10 flex justify-between">
                         <div className="flex w-[80%]">
-                            <input type="text" placeholder="Search" className="text-black p-1 pl-4 rounded-[3px] w-full"/>
-                            <Image src={search} alt="" className="w-5 h-5 m-2 relative right-8 cursor-pointer" id="search"/>
+                            <input type="text" placeholder="Search" className="text-black p-1 pl-4 rounded-[3px] w-full" />
+                            <Image src={search} alt="" className="w-5 h-5 m-2 relative right-8 cursor-pointer" id="search" />
                         </div>
                         <select className="rounded-[3px] p-1 w-[20%]">
                             <option value="topic1" hidden>Filter by topic</option>
@@ -44,12 +62,34 @@ export default function Home() {
                         </select>
                     </div>
                     <div className="flex flex-col justify-center items-center  gap-10 mt-16">
-                        <CardQuestion linkQuestion={"/question"} userPhoto={iconProfile.src} username={"Ingrid Rocha"} date={"12/12/2024"} topic={"Frontend"} question={"AAAA as fhsdjkfhsdjhgfjksdhgjs  sdjfbdsjbvjksdbv iasfeufsknvnsxmncz"} answers={0}/>
-                        <CardQuestion linkQuestion={"/question"} userPhoto={iconProfile.src} username={"Ingrid Rocha"} date={"12/12/2024"} topic={"Frontend"} question={"AAAA as fhsdjkfhsdjhgfjksdhgjs  sdjfbdsjbvjksdbv iasfeufsknvnsxmncz"} answers={0}/>
+                        <CardQuestion linkQuestion={"/question"} userPhoto={iconProfile.src} username={"Ingrid Rocha"} date={"12/12/2024"} topic={"Frontend"} question={"AAAA as fhsdjkfhsdjhgfjksdhgjs  sdjfbdsjbvjksdbv iasfeufsknvnsxmncz"} answers={0} />
+                        <CardQuestion linkQuestion={"/question"} userPhoto={iconProfile.src} username={"Ingrid Rocha"} date={"12/12/2024"} topic={"Frontend"} question={"AAAA as fhsdjkfhsdjhgfjksdhgjs  sdjfbdsjbvjksdbv iasfeufsknvnsxmncz"} answers={0} />
                     </div>
                 </div>
             </div>
 
+
+
+            {/* Modal */}
+            <div className={modal ? "fixed inset-0 flex items-center justify-center text-white bg-black bg-opacity-50 z-50" : "disabled z-0 opacity-0"}>
+                <div className="bg-zinc-800 p-8 rounded-lg shadow-lg flex items-center justify-center flex-col" >
+                    <div className="p-2 flex flex-col w-96 bg-opacity-50 z-50">
+                        <h2 className="text-xl font-semibold">New question</h2>
+                        <form className="flex flex-col">
+                            <label htmlFor="" className="mt-8">Title</label>
+                            <input type="text" placeholder="Forum title" className="border-2 rounded-[5px] p-1 mt-2 text-[13px]" value={title} onChange={(e) => { setTitle(e.target.value) }} ></input>
+                            <label htmlFor="" className="mt-8">Text</label>
+                            <input type="text" placeholder="Forum text" className="border-2 rounded-[5px] p-1 mt-2 text-[13px]" value={text} onChange={(e) => { setText(e.target.value) }} ></input>
+                            <label htmlFor="" className="mt-8">Topic ID</label>
+                            <input type="text" placeholder="Topic id" className="border-2 rounded-[5px] p-1 mt-2 text-[13px]" value={topic} onChange={(e) => { setTopic(e.target.value) }} ></input>
+                        </form>
+                        <div className="flex justify-between mt-10">
+                            <button onClick={() => closeModal()} className="flex justify-center items-center h-8 text-[15px] bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600">Cancelar</button>
+                            <button onClick={() => setModal(false)}className="flex justify-center items-center h-8 text-[15px] bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600">Confirm</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- Front/src/app/home/page.tsx
+++ Front/src/app/home/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { CardForum } from "@/components/cardForum";
 import { Menu } from "@/components/menu";
 import { Submenu } from "@/components/submenu";
@@ -18,28 +20,40 @@ const styles = {
 
 export default function Home() {
 
+    const [modal, setModal] = useState(false);
+    const [name, setName] = useState<string>("");
+
+    const closeModal = () => {
+        setName("");
+        setModal(false);
+    }
+
+    const openModal = () => {
+        setModal(true);
+    }
+
     return (
         <div>
-            <Menu title={"Ryse"}/>
-            <Submenu home={"Home"} chats={"Chats"} newGroup={"New group"} myGroup={"My groups"} chatPrincipal1={"Chat 1"} chatPrincipal2={"Chat 2"} chatPrincipal3={"Chat 3"} newIdea={"New idea"} ideas={"Ideas"}/>
+            <Menu title={"Ryse"} />
+            <Submenu home={"Home"} chats={"Chats"} newGroup={"New group"} myGroup={"My groups"} chatPrincipal1={"Chat 1"} chatPrincipal2={"Chat 2"} chatPrincipal3={"Chat 3"} newIdea={"New idea"} ideas={"Ideas"} />
             <div className="pt-[180px] pl-[300px] flex">
                 <div className="w-[75%]">
                     <div className="flex justify-between items-center">
                         <h2 className="text-white font-bold text-[25px] mb-6">Forum and Discussions</h2>
-                        <Link href={ROUTES.home} className={styles.button}>
-                            <Image src={iconMore} alt="ícone mais" className={styles.img}/>
+                        <button onClick={() => openModal()} className={styles.button}>
+                            <Image src={iconMore} alt="ícone mais" className={styles.img} />
                             New forum
-                        </Link>
+                        </button>
                     </div>
-                    <hr/>
+                    <hr />
                     <div className="w-[101.5%] pt-10 flex">
                         <div className="flex w-full justify-center items-center">
-                            <input type="text" placeholder="Search" className="text-black p-1.5 pl-4 rounded-[3px] w-[100%]"/>
-                            <Image src={search} alt="" className="w-5 h-5 relative right-8 cursor-pointer" id="search"/>
+                            <input type="text" placeholder="Search" className="text-black p-1.5 pl-4 rounded-[3px] w-[100%]" />
+                            <Image src={search} alt="" className="w-5 h-5 relative right-8 cursor-pointer" id="search" />
                         </div>
                     </div>
                     <div className="flex flex-col justify-center items-center gap-10 mt-16">
-                        <CardForum linkForum={"/forum"} userPhoto={iconProfile.src} username={"Ingrid Rocha"} date={"12/12/2024"} title={"Nome do forum"} questions={0}/>
+                        <CardForum linkForum={"/forum"} userPhoto={iconProfile.src} username={"Ingrid Rocha"} date={"12/12/2024"} title={"Nome do forum"} questions={0} />
                     </div>
                 </div>
 
@@ -55,6 +69,22 @@ export default function Home() {
                 </div>
             </div>
 
+            {/* Modal */}
+            <div className={modal ? "fixed inset-0 flex items-center justify-center text-white bg-black bg-opacity-50 z-50" : "disabled z-0 opacity-0"}>
+                <div className="bg-zinc-800 p-8 rounded-lg shadow-lg flex items-center justify-center flex-col" >
+                    <div className="p-2 flex flex-col w-96 bg-opacity-50 z-50">
+                        <h2 className="text-xl font-semibold">New forum</h2>
+                        <form className="flex flex-col">
+                            <label htmlFor="" className="mt-8">Name</label>
+                            <input type="text" placeholder="Forum name" className="border-2 rounded-[5px] p-1 mt-2 text-[13px]" value={name} onChange={(e) => { setName(e.target.value) }} ></input>
+                        </form>
+                        <div className="flex justify-between mt-10">
+                            <button onClick={() => closeModal()} className="flex justify-center items-center h-8 text-[15px] bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600">Cancelar</button>
+                            <button onClick={() => setModal(false)}className="flex justify-center items-center h-8 text-[15px] bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600">Confirm</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
