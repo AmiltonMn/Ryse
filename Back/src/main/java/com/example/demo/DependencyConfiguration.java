f295440be4ae2d413e2eedbf74ce3d94e06cf84d
WallyDev <wallace07abreu@gmail.com>
2024-12-09 08:18:33 -0300

    Add Dependencies

--- Back/src/main/java/com/example/demo/DependencyConfiguration.java
+++ Back/src/main/java/com/example/demo/DependencyConfiguration.java
@@ -6,13 +6,17 @@ import org.springframework.context.annotation.Configuration;
 import com.example.demo.DTO.Token;
 import com.example.demo.Filters.JWTAuthenticationFilter;
 import com.example.demo.Implementations.EncodeImplementations;
+import com.example.demo.Implementations.GroupImplementations;
 import com.example.demo.Implementations.HardSkillImplementation;
 import com.example.demo.Implementations.IdeaImplementations;
+import com.example.demo.Implementations.TopicImplementation;
 import com.example.demo.Implementations.UserImplementations;
 import com.example.demo.Services.EncodeServices;
+import com.example.demo.Services.GroupServices;
 import com.example.demo.Services.HardSkillService;
 import com.example.demo.Services.IdeaServices;
 import com.example.demo.Services.JWTService;
+import com.example.demo.Services.TopicService;
 import com.example.demo.Services.UserServices;
 
 @Configuration
