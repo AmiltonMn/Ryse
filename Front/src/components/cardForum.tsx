dbc83e1c281d909a743a0997779abefa748e7306
ingridcriistine <ingridrochacristine@gmail.com>
2024-12-11 11:08:03 -0300

    ajustes

--- Front/src/components/cardForum.tsx
+++ Front/src/components/cardForum.tsx
@@ -14,19 +14,18 @@ interface CardForumProps {
 export const CardForum: React.FC<CardForumProps> = ({linkForum, userPhoto, username, date, title, questions}) => {
     return(
         <Link href={linkForum} className="w-full">
-            <div className="bg-[#242424] rounded-[10px] text-white hover:bg-[#292929]">
-                <div className="flex justify-between items-center p-6">
+            <div className="bg-[#242424] rounded-[10px] text-white flex flex-col hover:bg-[#292929] p-6">
+                <div className="flex justify-between items-center">
                     <h3 className="text-[16px]">{title}</h3>
-                    <div className="flex p-2">
-                        <Image src={userPhoto} alt="ícone notificação" className="w-9 h-9 rounded-t-3xl m-2 mr-6" width={1000} height={1000}/>
-                        <div className="flex flex-col justify-center">
+                    <div className="flex mr-6">
+                        <Image src={userPhoto} alt="ícone notificação" className="w-9 h-9 rounded-t-3xl mr-6 mt-6" width={1000} height={1000}/>
+                        <div className="flex flex-col justify-center pt-6 gap-2">
                             <h4 className="text-[14px]">{username}</h4>
                             <p className="text-[12px]">{date}</p>
                         </div>
                     </div>
                 </div>
-                <p className="flex pl-6 pb-6 text-[14px]">{questions} questions</p>
-
+                <p className="flex pb-3 text-[12px]">{questions} questions</p>
             </div>
         </Link>
     );
