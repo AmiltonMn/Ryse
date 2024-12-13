1d5902c4270d676aff90381fb182e8223cc6e66b
adriansito124 <adriangobara@gmail.com>
2024-12-13 10:24:51 -0300

    fucoes do instrutor

--- Front/src/app/profile/page.tsx
+++ Front/src/app/profile/page.tsx
@@ -21,7 +21,7 @@ import search from "@/assets/lupa.png"
 
 const Profile: React.FC = () => {
 
-    
+    const [instrutor, setInstrutor] = useState(false)
     const [activeTab, setActiveTab] = useState("profile");
     const [text, setText] = useState("");
     const [feedbackTab, setFeedbackTab] = useState("received");
@@ -32,6 +32,10 @@ const Profile: React.FC = () => {
     const [name, setName] = useState<string>("");
     const [editBio, setEditBio] = useState(false)
     const [hardSkils, setHardSkils] = useState(false);
+
+    const tornar = () => {
+        setInstrutor(true)
+    }
 
     const [selectedSkills, setSelectedSkills] = useState<string[]>([]);
 
@@ -97,10 +101,18 @@ const Profile: React.FC = () => {
 
 
             <div className="pt-24 pl-[320px] pr-[70px] flex flex-col text-white">
-                <div className="font-medium text-[16px] flex flex-row pb-8">
-                    <SelectProfile refe="#" title="Profile" click={() => handleTabChange("profile")} classe={activeTab == "profile" ? "underline decoration-4" : ""} />
-                    <SelectProfile refe="#" title="FeedBacks" click={() => handleTabChange("feed")} classe={activeTab == "feed" ? "underline decoration-4" : ""} />
-                    <SelectProfile refe="#" title="Interaction" click={() => handleTabChange("interaction")} classe={activeTab == "interaction" ? "underline decoration-4" : ""} />
+                <div className="font-medium text-[16px] flex justify-between flex-row pb-8">
+                    <div className="flex">
+                        <SelectProfile refe="#" title="Profile" click={() => handleTabChange("profile")} classe={activeTab == "profile" ? "underline decoration-4" : ""} />
+                        <SelectProfile refe="#" title="FeedBacks" click={() => handleTabChange("feed")} classe={activeTab == "feed" ? "underline decoration-4" : ""} />
+                        <SelectProfile refe="#" title="Interaction" click={() => handleTabChange("interaction")} classe={activeTab == "interaction" ? "underline decoration-4" : ""} />
+                    </div>
+                    {
+                        instrutor ?
+                        <p className="text-[#F41C54] text-[16px] bg-white pl-2 pr-2 rounded-md font-semibold">Instrutor</p>
+                        :
+                        <button onClick={() => tornar()} className="bg-[#F41C54] hover:bg-white text-white hover:text-[#F41C54] transition-colors duration-500 delay-75 pl-2 pr-2 rounded-md">Tornar Instrutor</button>
+                    }
                 </div>
 
                 <div className="flex">
@@ -189,13 +201,13 @@ const Profile: React.FC = () => {
                         <form className="flex flex-col">
                             <div className="flex flex-wrap order-4 gap-4 pt-6">
 
-                            {/* Aqui implementação de comparção quando estive integrado */}
+                                {/* Aqui implementação de comparção quando estive integrado */}
 
-                            {hardSkills.map((skill) => (
-                                
-                                <SelectHardSkils text={skill} key={skill} click={handleSkillClick} classe={selectedSkills.includes(skill)}/>
+                                {hardSkills.map((skill) => (
 
-                            ))}
+                                    <SelectHardSkils text={skill} key={skill} click={handleSkillClick} classe={selectedSkills.includes(skill)} />
+
+                                ))}
                             </div>
                         </form>
                         <div className="flex justify-between mt-10">
